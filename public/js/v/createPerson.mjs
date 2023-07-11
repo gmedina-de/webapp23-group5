24f9c8b4b384a1158eb253150c04898ee2430790
sahajmajavdia <55780388+sahajmajavdia@users.noreply.github.com>
2023-07-11 14:34:26 +0200

    Update createPerson.mjs

--- public/js/v/createPerson.mjs
+++ public/js/v/createPerson.mjs
@@ -1,51 +1,50 @@
-/*Import Statements */
-
 import Person, { GenderEL } from "../m/Person.mjs";
-import { showProgressBar, hideProgressBar,fillSelectWithOptions } from "../../lib/util.mjs";
-import { handleAuthentication } from "./accessControl.mjs";
+import { fillSelectWithOptions } from "../../lib/util.mjs";
+import { showProgressBar, hideProgressBar } from "../../lib/util.mjs";
 
-/* UI Authentication */
-handleAuthentication();
 
-/* Variable Declaration */
 const formEl = document.forms["Person"],
-createButton = formEl["commit"],
-progressEl = document.querySelector("progress");
-
-/* Add event listeners */
-formEl["personId"].addEventListener("input", function () {
-    // do not yet check the ID constraint, only before commit
-    formEl["personId"].setCustomValidity( Person.checkPersonId( formEl["personId"].value).message);
-});
-formEl["personName"].addEventListener("input", function () {
-    formEl["personName"].setCustomValidity( Person.checkName( formEl["personName"].value).message);
-});
-formEl["birth"].addEventListener("input", function () {
-    formEl["birth"].setCustomValidity( Person.checkBirth( formEl["birth"].value).message);
-});
-
+    createButton = formEl["commit"],
+    progressEl = document.querySelector("progress");
 
 const createGenderSelectEl = formEl.gender;
 fillSelectWithOptions(createGenderSelectEl, GenderEL.labels);
 
+formEl["personId"].addEventListener("input", function () {
+    formEl["personId"].setCustomValidity(Person.checkPersonId(formEl["personId"].value).message);
+    formEl["personId"].reportValidity();
+});
+formEl["personName"].addEventListener("input", function () {
+    formEl["personName"].setCustomValidity(Person.checkPersonName(formEl["personName"].value).message);
+    formEl["personName"].reportValidity();
+});
+formEl["gender"].addEventListener("input", function () {
+    formEl["gender"].setCustomValidity(Person.checkGender(formEl["gender"].value).message);
+    formEl["gender"].reportValidity();
+});
+formEl["birth"].addEventListener("input", function () {
+    formEl["birth"].setCustomValidity(Person.checkBirth(formEl["birth"].value).message);
+    formEl["birth"].reportValidity();
+});
 createButton.addEventListener("click", async function () {
- const slots = {
-   personId: formEl["personId"].value,
-   personName: formEl["personName"].value,
-   birth: formEl["birth"].value,
-   gender: formEl["gender"].value,
- };
-    showProgressBar( progressEl);
-    formEl["personId"].setCustomValidity(( await Person.checkPersonIdAsId( slots.personId)).message);
-    formEl["personName"].setCustomValidity( Person.checkName( slots.personName).message);
-    formEl["birth"].setCustomValidity( Person.checkBirth( slots.birth).message);
+    const slots = {
+        personId: formEl["personId"].value,
+        personName: formEl["personName"].value,
+        gender: formEl["gender"].value,
+        birth: formEl["birth"].value,
+    };
+    showProgressBar(progressEl);
+    formEl["personId"].setCustomValidity((await Person.checkPersonIdAsId(slots.personId)).message);
+    formEl["personId"].reportValidity();
+    formEl["personName"].setCustomValidity(Person.checkPersonName(slots.personName).message);
+    formEl["gender"].setCustomValidity(Person.checkGender(slots.gender).message);
+    formEl["birth"].setCustomValidity(Person.checkBirth(slots.birth).message);
     if (formEl.checkValidity()) {
-        await Person.add( slots);
+        await Person.add(slots);
         formEl.reset();
     }
-    hideProgressBar( progressEl);
+    hideProgressBar(progressEl);
 });
-// neutralize the submit event
 formEl.addEventListener("submit", function (e) {
     e.preventDefault();
 });
