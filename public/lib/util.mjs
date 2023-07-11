60fa7630b84d29b5fee9db8a0544086a338c4e81
sahajmajavdia <55780388+sahajmajavdia@users.noreply.github.com>
2023-07-11 15:03:31 +0200

    Update util.mjs

--- public/lib/util.mjs
+++ public/lib/util.mjs
@@ -1,106 +1,167 @@
-/**
- * @fileOverview  Defines utility procedures/functions
- * @author Gerd Wagner
- * @author Juan-Francisco Reyes
- */
-
-/**
- * Verifies if a value represents an integer
- * @param {string} x
- * @return {boolean}
- */
-function isNonEmptyString( x) {
+function isNonEmptyString(x) {
   return typeof (x) === "string" && x.trim() !== "";
 }
-
-/**
- * Return the next year value (e.g. if now is 2013 the function will return 2014)
- * @return {number}  the integer representing the next year value
- */
 function nextYear() {
   const date = new Date();
   return (date.getFullYear() + 1);
 }
 
-/**
- * Convert Firestore timeStamp object to Date string in format YYYY-MM-DD
- * @param {object} timeStamp A Firestore timeStamp object
- */
-function date2IsoDateString (timeStamp) {
-  const dateObj = timeStamp.toDate();
-  let  y = dateObj.getFullYear(),
-      m = "" + (dateObj.getMonth() + 1),
-      d = "" + dateObj.getDate();
-  if (m.length < 2) m = "0" + m;
-  if (d.length < 2) d = "0" + d;
-  return [y, m, d].join("-");
+function isIntegerOrIntegerString(x) {
+  return typeof (x) === "number" && Number.isInteger(x) ||
+    typeof (x) === "string" && x.search(/^-?[0-9]+$/) === 0;
 }
-
-/**
- * Verifies if a value represents an integer or integer string
- * @param {string} x
- * @return {boolean}
- */
-function isIntegerOrIntegerString( x) {
-  return typeof (x) === "number" && x.toString().search(/^-?[0-9]+$/) === 0 ||
-      typeof (x) === "string" && x.search(/^-?[0-9]+$/) === 0;
-}
-
-/**
- * Creates a typed "data clone" of an object
- * @param {object} obj
- */
-function cloneObject( obj) {
-  const clone = Object.create( Object.getPrototypeOf( obj));
-  for (const p in obj) {
-    if (obj.hasOwnProperty(p) && typeof obj[p] !== "object") clone[p] = obj[p];
+function cloneObject(obj) {
+  let p = "", val,
+    clone = Object.create(Object.getPrototypeOf(obj));
+  for (p in obj) {
+    if (obj.hasOwnProperty(p)) {
+      val = obj[p];
+      if (typeof val === "number" ||
+        typeof val === "string" ||
+        typeof val === "boolean" ||
+        val instanceof Date ||
+        // typed object reference
+        typeof val === "object" && !!val.constructor ||
+        // list of data values
+        Array.isArray(val) &&
+        !val.some(function (el) {
+          return typeof el === "object";
+        }) ||
+        // list of typed object references
+        Array.isArray(val) &&
+        val.every(function (el) {
+          return (typeof el === "object" && !!el.constructor);
+        })
+      ) {
+        if (Array.isArray(val)) clone[p] = val.slice(0);
+        else clone[p] = val;
+      }
+      // else clone[p] = cloneObject(val);
+    }
   }
   return clone;
 }
 
-/** Create option elements from a map of objects
- * and insert them into a selection list element
- *
- * @param {object} objColl  A collection (list or map) of objects
- * @param {object} selEl  A select(ion list) element
- * @param {string} stdIdProp  The standard identifier property
- * @param {string} displayProp [optional]  A property supplying the text
- *                 to be displayed for each object
- */
-function fillSelectWithOptions( objColl, selEl, stdIdProp, displayProp) {
-  if (Array.isArray(objColl)) {
-    for (const obj of objColl) {
-      const optionEl = document.createElement("option");
-      optionEl.value = obj[stdIdProp];
-      optionEl.text = displayProp ? obj[displayProp] : obj[stdIdProp];
-      selEl.add(optionEl, null)
+function createOption(val, txt, classValues) {
+  const el = document.createElement("option");
+  el.value = val;
+  el.text = txt || val;
+  if (classValues) el.className = classValues;
+  return el;
+}
+function fillSelectWithOptions(selectEl, selectionRange, optPar) {
+  // create option elements from array key and values
+  const options = selectionRange.entries();
+  // delete old contents
+  selectEl.innerHTML = "";
+  // create "no selection yet" entry
+  if (!selectEl.multiple) {
+    selectEl.add(createOption("", " --- "));
+  }
+  //
+  for (const [index, o] of options) {
+    const key = index + 1;
+    const optionEl = createOption(
+      optPar ? (o[optPar.valueProp] ? o[optPar.valueProp] : key) : key,
+      optPar ? (o[optPar.displayProp] ? o[optPar.displayProp] : o) : o
+    );
+    if (selectEl.multiple && optPar && optPar.selection &&
+      optPar.selection.includes(key)) {
+      // flag the option element with this value as selected
+      optionEl.selected = true;
     }
-  } else {
-    for (const key of Object.keys( objColl)) {
-      const obj = objColl[key];
-      const optionEl = document.createElement("option");
-      optionEl.value = obj[stdIdProp];
-      optionEl.text = displayProp ? obj[displayProp] : obj[stdIdProp];
-      selEl.add(optionEl, null)
-    }
+    selectEl.add(optionEl);
   }
 }
-
-/**
- * Show progress bar element
- * @param {object} progressEl
- */
-function showProgressBar (progressEl) {
-  progressEl.hidden = false;
+function createLabeledChoiceControl(t, n, v, lbl) {
+  const ccEl = document.createElement("input"),
+    lblEl = document.createElement("label");
+  ccEl.type = t;
+  ccEl.name = n;
+  ccEl.value = v;
+  lblEl.appendChild(ccEl);
+  lblEl.appendChild(document.createTextNode(lbl));
+  return lblEl;
+}
+function createChoiceWidget(containerEl, fld, values,
+  choiceWidgetType, choiceItems, isMandatory) {
+  const choiceControls = containerEl.querySelectorAll("label");
+  // remove old content
+  for (const j of choiceControls.keys()) {
+    containerEl.removeChild(choiceControls[j]);
+  }
+  if (!containerEl.hasAttribute("data-bind")) {
+    containerEl.setAttribute("data-bind", fld);
+  }
+  // for a mandatory radio button group initialze to first value
+  if (choiceWidgetType === "radio" && isMandatory && values.length === 0) {
+    values[0] = 1;
+  }
+  if (values.length >= 1) {
+    if (choiceWidgetType === "radio") {
+      containerEl.setAttribute("data-value", values[0]);
+    } else {  // checkboxes
+      containerEl.setAttribute("data-value", "[" + values.join() + "]");
+    }
+  }
+  for (const j of choiceItems.keys()) {
+    // button values = 1..n
+    const el = createLabeledChoiceControl(choiceWidgetType, fld,
+      j + 1, choiceItems[j]);
+    // mark the radio button or checkbox as selected/checked
+    if (values.includes(j + 1)) el.firstElementChild.checked = true;
+    containerEl.appendChild(el);
+    el.firstElementChild.addEventListener("click", function (e) {
+      const btnEl = e.target;
+      if (choiceWidgetType === "radio") {
+        if (containerEl.getAttribute("data-value") !== btnEl.value) {
+          containerEl.setAttribute("data-value", btnEl.value);
+        } else if (!isMandatory) {
+          // turn off radio button
+          btnEl.checked = false;
+          containerEl.setAttribute("data-value", "");
+        }
+      } else {  // checkbox
+        let values = JSON.parse(containerEl.getAttribute("data-value")) || [];
+        let i = values.indexOf(parseInt(btnEl.value));
+        if (i > -1) {
+          values.splice(i, 1);  // delete from value list
+        } else {  // add to value list
+          values.push(btnEl.value);
+        }
+        containerEl.setAttribute("data-value", "[" + values.join() + "]");
+      }
+    });
+  }
+  return containerEl;
 }
 
-/**
- * Hide progress bar element
- * @param {object} progressEl
- */
-function hideProgressBar (progressEl) {
+function showProgressBar(progressEl) {
+  progressEl.hidden = false;
+}
+function hideProgressBar(progressEl) {
   progressEl.hidden = true;
 }
 
-export { isNonEmptyString, nextYear, isIntegerOrIntegerString, cloneObject,
-  fillSelectWithOptions, showProgressBar, hideProgressBar };
+function createModalFromChange(change) {
+  const { itemName, description, type } = change,
+    divModalWindowEl = document.querySelector("#modal-window"),
+    divModalContentEl = divModalWindowEl.querySelector("div"),
+    pEl = document.createElement("p"),
+    btnEl = document.createElement("button");
+  divModalContentEl.innerHTML = "";
+  pEl.textContent = `The selected ${itemName} "${description}" has been ${type} by another user.`;
+  btnEl.type = "button";
+  btnEl.textContent = "Reload this page to continue";
+  btnEl.addEventListener("click", () => location.reload());
+  divModalContentEl.appendChild(pEl);
+  divModalContentEl.appendChild(btnEl);
+  divModalWindowEl.appendChild(divModalContentEl);
+  divModalWindowEl.classList.add("show-modal");
+}
+
+export {
+  isNonEmptyString, isIntegerOrIntegerString, createOption,
+  fillSelectWithOptions, createChoiceWidget, showProgressBar, hideProgressBar,
+  createModalFromChange
+};
