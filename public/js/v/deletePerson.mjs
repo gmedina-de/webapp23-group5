85ed58acf50b8ce748ab2b9055c5dbcd2aeac1f5
sahajmajavdia <55780388+sahajmajavdia@users.noreply.github.com>
2023-07-11 14:34:57 +0200

    Update deletePerson.mjs

--- public/js/v/deletePerson.mjs
+++ public/js/v/deletePerson.mjs
@@ -1,27 +1,31 @@
-import Person from "../m/Person.mjs";
-import { handleAuthentication } from "./accessControl.mjs";
-import { fillSelectWithOptions } from "../../lib/util.mjs";
+ import Person from "../m/Person.mjs";
+ const PersonRecords = await Person.retrieveAll();
 
-handleAuthentication();
-const PersonRecords = await Person.retrieveAll();
+ const formEl = document.forms["Person"],
+   deleteButton = formEl["commit"],
+   selectPersonEl = formEl["selectPerson"];
+ let cancelListener = null;
 
-const formEl = document.forms["Person"],
-  deleteButton = formEl["commit"],
-  selectPersonEl = formEl["selectPerson"];
+ for (const PersonRec of PersonRecords) {
+   const optionEl = document.createElement("option");
+   optionEl.text = PersonRec.personName;
+   optionEl.value = PersonRec.personId;
+   selectPersonEl.add(optionEl, null);
+ }
 
-fillSelectWithOptions(PersonRecords,selectPersonEl,"personId", "personName")
-// for (const PersonRec of PersonRecords) {
-//   const optionEl = document.createElement("option");
-//   optionEl.text = PersonRec.personName;
-//   optionEl.value = PersonRec.personId;
-//   selectPersonEl.add(optionEl, null);
-// }
-
-deleteButton.addEventListener("click", async function () {
-  const personId = selectPersonEl.value;
-  if (!personId) return;
-  if (confirm("Do you really want to delete this Person?")) {
-    await Person.destroy(personId);
-    selectPersonEl.remove(selectPersonEl.selectedIndex);
-  }
-});
+ selectPersonEl.addEventListener("change", async function () {
+   const personKey = selectPersonEl.value;
+   if (personKey) {
+     if (cancelListener) cancelListener();
+     cancelListener = await Person.observeChanges(personKey);
+   }
+ });
+ 
+ deleteButton.addEventListener("click", async function () {
+   const personId = selectPersonEl.value;
+   if (!personId) return;
+   if (confirm("Do you really want to delete this Person record?")) {
+     await Person.destroy(personId);
+     selectPersonEl.remove(selectPersonEl.selectedIndex);
+   }
+ });
